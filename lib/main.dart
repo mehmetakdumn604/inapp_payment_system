c2aeceb2ce58be4228e688b2a820d917fb87db14
Mehmet Akduman <makdumn604@gmail.com>
2022-07-07 00:42:14 +0300

    init payment module

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:inapp_payment_system/services/payment_service.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+    this.notAvailableTxt = "This store is not available.",
+    required this.productWidget,
+    required this.onTapToBuy,
+  }) : super(key: key);
+
+  final String notAvailableTxt;
+  final Widget productWidget;
+  final Function(Offerings offerings) onTapToBuy;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<bool>(
+        future: PaymentService.isConfigured(),
+        builder: (context, isConfigured) {
+          if (!isConfigured.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (isConfigured.data == null || !isConfigured.data!) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  notAvailableTxt,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+          return FutureBuilder<Offerings>(
+              future: PaymentService.getOfferings(),
+              builder: (context, offeringsSnapshot) {
+                if (!offeringsSnapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (offeringsSnapshot.data!.current == null) {
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        notAvailableTxt,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }
+                return productWidget;
+              });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/services/payment_service.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/services.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+class PaymentService {
+  static Future<void> purchaseProduct(String productId) async {
+    try {
+      PurchaserInfo purchaserInfo = await Purchases.purchaseProduct(productId);
+    } on PlatformException catch (e) {
+      var errorCode = PurchasesErrorHelper.getErrorCode(e);
+      if (errorCode != PurchasesErrorCode.purchaseCancelledError) {
+        print(e);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future<void> purchasePackage(Package purchasePackage) async {
+    try {
+      PurchaserInfo purchaserInfo =
+          await Purchases.purchasePackage(purchasePackage);
+    } on PlatformException catch (e) {
+      var errorCode = PurchasesErrorHelper.getErrorCode(e);
+      if (errorCode != PurchasesErrorCode.purchaseCancelledError) {
+        print(e);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future<Offerings> getOfferings() async {
+    try {
+      return await Purchases.getOfferings();
+    } on PlatformException catch (e) {
+      var errorCode = PurchasesErrorHelper.getErrorCode(e);
+      if (errorCode != PurchasesErrorCode.purchaseCancelledError) {
+        print(e);
+      }
+      throw Exception("offerings PlatformException hatası");
+    } catch (e) {
+      print(e);
+      throw Exception("offerings catch hatası");
+    }
+  }
+
+  static Future<List<Product>> getProducts(List<String> productIds) async {
+    try {
+      return await Purchases.getProducts(productIds);
+    } on PlatformException catch (e) {
+      var errorCode = PurchasesErrorHelper.getErrorCode(e);
+      if (errorCode != PurchasesErrorCode.purchaseCancelledError) {
+        print(e);
+      }
+      throw Exception("offerings PlatformException hatası");
+    } catch (e) {
+      print(e);
+      throw Exception("offerings catch hatası");
+    }
+  }
+
+  static Future<bool> isConfigured() async {
+    try {
+      return await Purchases.isConfigured;
+    } catch (e) {
+      throw Exception("not configured");
+    }
+  }
+}
